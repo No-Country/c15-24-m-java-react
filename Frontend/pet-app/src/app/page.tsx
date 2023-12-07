13d145c496cf86fa321994449c624e27d08ceefd
MemoSan27 <135660314+MemoSan27@users.noreply.github.com>
2023-12-07 13:23:22 -0700

    redirect added

--- Frontend/pet-app/src/app/page.tsx
+++ Frontend/pet-app/src/app/page.tsx
@@ -1,22 +1,5 @@
-import Form from "@/components/loginForm/form";
-import Image from "next/image";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main className="min-h-screen grid grid -cols-1 lg:grid-cols-2 p-5 lg:p-8  ">
-      <section className="h-full flex felx-col items-center justify-center bg-primary rounded-2xl">
-        <div className="relative w-96 h-96 p-5">
-          <Image src="/wallpaper.svg" alt="wallpaper" fill />
-        </div>
-        <div>
-          <h3 className="text-white text-xl font-semibold text-center">
-            Bienvenidos a PetApp
-          </h3>
-        </div>
-      </section>
-      <section className="h-full flex flex col items-center justify-center">
-        <Form />
-      </section>
-    </main>
-  );
+  redirect('/login')
 }
