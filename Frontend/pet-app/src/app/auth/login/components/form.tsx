43055cddca334817e5f5028f14f4f71fee7961ed
F4lleau <ivonnefalleau@gmail.com>
2023-12-06 22:45:27 -0300

    primer commit a mi rama

---
+++ Frontend/pet-app/src/app/auth/login/components/form.tsx
@@ -0,0 +1,22 @@
+import Input from "@/components/ui/input";
+
+const Form =() =>{
+ return (
+    <div className= 'w-full max-w-md'>
+        <div className= 'mb-5'>
+            <h2 className= 'text-gray-500 font-semibold text- justify-center'>Por favor, ingrese su mail y contraseña</h2>
+        </div>
+        <form className= 'w-full'>
+            <Input type='text' placeholder='Email'/>
+            <Input type='password' placeholder='Password'/>
+            <div className ='flex justify-end '>
+                <button type= 'button' className='text-grey-500'>
+                    Olvidaste la contraseña?
+                </button>
+            </div>
+        </form>
+   </div>
+ )
+}
+
+export default Form;
---
+++ Frontend/pet-app/src/app/auth/login/page.tsx
@@ -0,0 +1,8 @@
+import Form from './components/form';
+
+export default function Login() {
+  return(
+  <section className= 'h-full flex flex col items-center justify-center'>
+    <Form/>
+  </section>)
+}
