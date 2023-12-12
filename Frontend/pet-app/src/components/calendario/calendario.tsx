efc574a3f07fc07b602788b380d649488f964a05
Nahuel Benitez <nahueljosebenitez7@gmail.com>
2023-12-11 23:34:23 -0300

    feat: react-calendar styles

--- Frontend/pet-app/src/components/calendario/calendario.tsx
+++ Frontend/pet-app/src/components/calendario/calendario.tsx
@@ -2,6 +2,7 @@
 
 import "../calendario/calendario.css";
 import Calendar from "react-calendar";
+import { useEffect, useState } from "react";
 
 export const CalendarioComponent = () => {
   const fechasMarcadas = [
@@ -11,9 +12,39 @@ export const CalendarioComponent = () => {
     "2024-04-16",
   ];
 
+  const [date, setDate] = useState(new Date());
+  const [month, setMonth] = useState(
+    date.toLocaleString("default", { month: "long" })
+  );
+  const [year, setYear] = useState(date.getFullYear());
+
+  useEffect(() => {
+    setMonth(date.toLocaleString("default", { month: "long" }));
+  }, [date]);
+
+  const changeMonthPrevious = () => {
+    setDate(new Date(date.getFullYear(), date.getMonth() - 1, date.getDate()));
+    setMonth(date.toLocaleString("default", { month: "long" }));
+    setYear(date.getFullYear());
+  };
+
+  const changeMonthNext = () => {
+    setDate(new Date(date.getFullYear(), date.getMonth() + 1, date.getDate()));
+    setMonth(date.toLocaleString("default", { month: "long" }));
+    setYear(date.getFullYear());
+  };
+
   return (
     <div className={"containerCalendario"}>
+      <div className="navigation_calendar">
+        <p className="navigate_title">
+          {month} {year}
+        </p>
+        <button onClick={changeMonthPrevious}>prev</button>
+        <button onClick={changeMonthNext}>next</button>
+      </div>
       <Calendar
+        value={date}
         tileClassName={({ date, view }) => {
           // Convertir la fecha a un string en el formato 'YYYY-MM-DD'
           const fechaString = date.toISOString().split("T")[0];
