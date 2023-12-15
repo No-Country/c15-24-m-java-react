47d40ca72dacc6895990f249fe3cbf287fe84085
dalmiro <dalmiropintos@gmail.com>
2023-12-15 14:07:12 -0300

    Eliminando directorios y archivos superfluos

--- Backend/pet-app/src/main/java/com/c1524m/petapp/exceptions/PetNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.c1524m.petapp.exceptions;
-
-public class PetNotFoundException  extends RuntimeException {
-
-    public PetNotFoundException() {
-        super("La mascota ingresada no existe.");
-    }
-}
