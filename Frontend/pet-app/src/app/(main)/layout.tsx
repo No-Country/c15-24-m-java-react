ce3d2c5f90e4aa0ad74485cec920dd3ad375d640
nicolas gonzalez <gonzaleznicolas8000@gmail.com>
2023-12-19 17:02:40 -0300

    section_mascotas

--- Frontend/pet-app/src/app/(main)/layout.tsx
+++ Frontend/pet-app/src/app/(main)/layout.tsx
@@ -4,8 +4,7 @@ import "../../app/globals.css";
 import { Navbar } from "../../components/navbar/navbar";
 import { ContainerMain } from "../../components/containerMain/containerMain";
 import { NavbarSup } from "@/components/navbarSuperior/navbarSup";
-
-
+import { DataProvider } from "@/components/Context/DataContext";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -14,20 +13,23 @@ export const metadata: Metadata = {
     "aplicacion para gestionar las tareas que conlleva el cuidado de las mascotas",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+
+
+export default function RootLayout({children,}: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-
        
+
+        <DataProvider>
+
+   
 
         <Navbar></Navbar>
         <NavbarSup />
         <ContainerMain>{children}</ContainerMain>
+  
+        </DataProvider>
         
       </body>
     </html>
--- Frontend/pet-app/src/components/NuevoGasto/NuevoGasto.tsx
+++ Frontend/pet-app/src/components/NuevoGasto/NuevoGasto.tsx
@@ -3,27 +3,26 @@ import { useState } from "react";
 import Image from "next/image";
 import agregar from "../../../public/images/agregar.svg";
 import Formulario from "../Formulario/Formulario";
-import close from "../../../public/images/Close.svg";
-import tres from "../../../public/images/tresPuntos.svg";
+
 import redondo from "../../../public/images/redondo.svg";
 import styles from "../NuevoGasto/NuevoGasto.module.css";
 import CardContact from "./CardContact/CardContact";
+import { Animal } from "../interface/interface";
+import { useDataContext } from "../Context/DataContext";
+import Controlador from "./Controlador/Controlador";
 
 
-export interface Animal {
-  especie: string;
-  raza: string;
-  color: string;
-  edad: number;
-  sexo: string;
-  esterilizado: string;
-  tratamiento: string;
-  alergias: string;
-}
 
 export const NuevoGasto = () => {
-  const [addGasto, setAddgasto] = useState<boolean>(false);
+  const { setData, addCard, setAlerta, alerta,addGasto,setAddgasto,setImage } = useDataContext();
+
+
+ 
+
+
+
   const [datos, setDatos] = useState<Animal>({
+    nombre: "",
     especie: "",
     raza: "",
     color: "",
@@ -32,10 +31,12 @@ export const NuevoGasto = () => {
     esterilizado: "",
     tratamiento: "",
     alergias: "",
+    mensaje: "",
+  
   });
 
-  const [data, setData] = useState<Animal | null>(null);
-  const [alerta, setAlerta] = useState("");
+
+
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
@@ -44,19 +45,47 @@ export const NuevoGasto = () => {
     const valoresDatos = Object.values(datos);
 
     if (valoresDatos.includes("") || datos.edad === 0) {
-      setAlerta("Por favor, verifica que todos los campos estén llenos.");
+      setAlerta(true);
       setTimeout(() => {
-        setAlerta("");
+        setAlerta(!true);
       }, 2000);
     } else {
       setData(datos);
-    }
+      addCard(datos);
 
-    //
+      // limpiar formulario
+      setDatos({
+        nombre: "",
+
+        raza: "",
+        color: "",
+        edad: 0,
+        sexo: "",
+        esterilizado: "",
+        tratamiento: "",
+        alergias: "",
+        mensaje: "",
+        especie: "",
+     
+      });
+
+      // cerrar Formulario
+      setAddgasto(false);
+    }
   };
+
+  // Capturar Datos de los input
 
   const handleChangeDatos = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
+
+
+    
+
+
+
+
+
 
     if (name === "edad") {
       const newValue = parseInt(value, 10); // Convertir a número entero con base 10
@@ -72,215 +101,238 @@ export const NuevoGasto = () => {
     }
   };
 
+  // Datos de Textarea
+  const handleChangeTextArea = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setDatos({
+      ...datos,
+      [name]: value,
+    });
+  };
+
   const toggleForm = (): void => {
     setAddgasto(!addGasto);
   };
 
+
+
   return (
-  
-      <div className="">
-        <div className="btn fixed bottom-10 right-8 cursor-pointer z-20">
-          <Image
-            onClick={toggleForm}
-            className="h-20 w-20  "
-            alt="boton_agregar"
-            src={agregar}
-          ></Image>
-        </div>
+    <div className="">
+      <div className="btn fixed bottom-10 right-8 cursor-pointer z-20">
+        <Image
+          onClick={toggleForm}
+          className="h-20 w-20  "
+          alt="boton_agregar"
+          src={agregar}
+        ></Image>
+      </div>
 
-        <div className="bg-black text-center text-white flex flex-col justify-center">
-          <h2>Especie: {data?.especie}</h2>
-          <h2>Raza: {data?.raza}</h2>
-          <h2>Color: {data?.color}</h2>
-          <h2>Edad: {data?.edad}</h2>
-          <h2>Sexo: {data?.sexo}</h2>
-          <h2>Esterilizado: {data?.esterilizado}</h2>
-          <h2>Tratamiento: {data?.tratamiento}</h2>
-          <h2>Alergias: {data?.alergias}</h2>{" "}
-        </div>
+      <div className="bg-black text-center text-white flex flex-col justify-center"></div>
 
-        {addGasto && (
-          <>
-            <Formulario datos={datos}>
-              <div className={`${styles.contenedor_form} `}>
-                <div className="add_file">
-                  <label
-                    htmlFor="file-upload"
-                    className={`${styles.fileUpload}`}
-                  >
-                    Agregar foto
-                  </label>
+      {addGasto && (
+        <>
+       
+     
 
-                  <input
-                    id="file-upload"
-                    className={`${styles.fileInput}`}
-                    type="file"
-                  />
-                </div>
+          <Formulario >
 
-                <div className="content">
-                  <div className="nombre  w-full">
-                    <h2 className={`${styles.titularLabel}`}>Nombre</h2>
+          <form className={`${styles.animationForm}  `}>
 
-                    <div className="grid grid-cols-2">
-                      <div className="izq">
-                        <div className="redondo  gap-2 flex">
-                          <Image alt="redondo" src={redondo}></Image>
-                          <label htmlFor="">Especie</label>
-                        </div>
-                        <input
-                          onChange={(e) => handleChangeDatos(e)}
-                          name="especie"
-                          className="bg-gray-100 text-black w-36 text-md"
-                          type="text"
-                        />
+            <div className={`${styles.contenedor_form} `}>
+              <div className="add_file">
+                <label htmlFor="file-upload" className={`${styles.fileUpload}`}>
+                  Agregar foto
+                </label>
 
-                        <div className="redondo  gap-2 flex">
-                          <Image alt="redondo" src={redondo}></Image>
-                          <label htmlFor="">Raza</label>
-                        </div>
+                <input
+                  id="file-upload"
+                  className={`${styles.fileInput}`}
+                  type="file"
+                  onChange={handleChangeDatos}
 
-                        <input
-                          onChange={(e) => handleChangeDatos(e)}
-                          className="bg-gray-200 w-36"
-                          name="raza"
-                          type="text"
-                        />
+                />
+              </div>
 
-                        <div className="redondo  gap-2 flex">
-                          <Image alt="redondo" src={redondo}></Image>
-                          <label htmlFor="">Color</label>
-                        </div>
-                        <input
-                          onChange={(e) => handleChangeDatos(e)}
-                          className="bg-gray-200 w-36"
-                          name="color"
-                          type="text"
-                        />
-                      </div>
+              <div className="content">
+                <div className="nombre  w-full">
+                  <h2 className={`${styles.titularLabel} `}>
+                    Información de la mascota
+                  </h2>
 
-                      <div className="derecha">
-                        <div className="redondo gap-2 flex">
-                          <Image alt="redondo" src={redondo}></Image>
-                          <label htmlFor="">Edad</label>
-                        </div>
-
-                        <input
-                          onChange={(e) => handleChangeDatos(e)}
-                          className="bg-gray-200 w-36"
-                          name="edad"
-                          type="number"
-                        />
-
-                        <div className="redondo  gap-2 flex">
-                          <Image alt="redondo" src={redondo}></Image>
-                          <label htmlFor="">Sexo</label>
-                        </div>
-
-                        <input
-                          onChange={(e) => handleChangeDatos(e)}
-                          className="bg-gray-200 w-36"
-                          name="sexo"
-                          type="text"
-                        />
-                      </div>
-                    </div>
-                  </div>
-
-                  <h2 className={`${styles.titularLabel} relative `}>Salud</h2>
-
-                  <div className="grid grid-cols-2 ">
+                  <div className="grid grid-cols-2">
                     <div className="izq">
-                      <div className="redondo flex gap-2">
+                      <div className="redondo  gap-2 flex">
                         <Image alt="redondo" src={redondo}></Image>
-                        <label htmlFor="">Esterilizado</label>
+                        <label htmlFor="">Nombre</label>
                       </div>
+
                       <input
                         onChange={(e) => handleChangeDatos(e)}
-                        className="bg-gray-100 w-36"
-                        name="esterilizado"
+                        name="nombre"
+                        className="w-36  py-1 rounded-md border border-gray-300 focus:outline-none focus:ring focus:border-blue-500"
                         type="text"
                       />
 
                       <div className="redondo  gap-2 flex">
                         <Image alt="redondo" src={redondo}></Image>
-                        <label htmlFor="">Alergias</label>
+                        <label htmlFor="">Raza</label>
                       </div>
 
                       <input
                         onChange={(e) => handleChangeDatos(e)}
-                        className="bg-gray-200 w-36"
-                        name="alergias"
+                        className="w-36  py-1 rounded-md border border-gray-300 focus:outline-none focus:ring focus:border-blue-500"
+                        name="raza"
+                        type="text"
+                      />
+
+                      <div className="redondo  gap-2 flex">
+                        <Image alt="redondo" src={redondo}></Image>
+                        <label htmlFor="">Color</label>
+                      </div>
+                      <input
+                        onChange={(e) => handleChangeDatos(e)}
+                        className="w-36  py-1 rounded-md border border-gray-300 focus:outline-none focus:ring focus:border-blue-500"
+                        name="color"
                         type="text"
                       />
                     </div>
 
                     <div className="derecha">
-                      <div className="redondo  gap-2 flex">
+                      <div className="redondo gap-2 flex">
                         <Image alt="redondo" src={redondo}></Image>
-                        <label htmlFor="">Tratamiento</label>
+                        <label htmlFor="">Edad</label>
                       </div>
 
                       <input
                         onChange={(e) => handleChangeDatos(e)}
-                        className="bg-gray-200 w-36"
-                        name="tratamiento"
+                        className="w-36  py-1 rounded-md border border-gray-300 focus:outline-none focus:ring focus:border-blue-500"
+                        name="edad"
+                        type="number"
+                      />
+
+                      <div className="redondo  gap-2 flex">
+                        <Image alt="redondo" src={redondo}></Image>
+                        <label htmlFor="">Sexo</label>
+                      </div>
+
+                      <input
+                        onChange={(e) => handleChangeDatos(e)}
+                        className="w-36  py-1 rounded-md border border-gray-300 focus:outline-none focus:ring focus:border-blue-500"
+                        name="sexo"
+                        type="text"
+                      />
+
+                      <div className="redondo  gap-2 flex">
+                        <Image alt="redondo" src={redondo}></Image>
+                        <label>Especie</label>
+                      </div>
+
+                      <input
+                        onChange={(e) => handleChangeDatos(e)}
+                        className="w-36  py-1 rounded-md border border-gray-300 focus:outline-none focus:ring focus:border-blue-500"
+                        name="especie"
                         type="text"
                       />
                     </div>
                   </div>
+                </div>
 
-                  <h2 className={`${styles.titularLabel} relative`}>
-                    Alimentacion
-                  </h2>
-                  <div className="w-full">
-                    <textarea
-                      className={`${styles.textarea}`}
-                      name=""
-                      id=""
-                    ></textarea>
+                <h2 className={`${styles.titularLabel} relative `}>Salud</h2>
+
+                <div className="grid grid-cols-2 ">
+                  <div className="izq">
+                    <div className="redondo flex gap-2">
+                      <Image alt="redondo" src={redondo}></Image>
+                      <label htmlFor="">Esterilizado</label>
+                    </div>
+                    <input
+                      onChange={(e) => handleChangeDatos(e)}
+                      className="w-36  py-1 rounded-md border border-gray-300 focus:outline-none focus:ring focus:border-blue-500"
+                      name="esterilizado"
+                      type="text"
+                    />
+
+                    <div className="redondo  gap-2 flex">
+                      <Image alt="redondo" src={redondo}></Image>
+                      <label htmlFor="">Alergias</label>
+                    </div>
+
+                    <input
+                      onChange={(e) => handleChangeDatos(e)}
+                      className="w-36  py-1 rounded-md border border-gray-300 focus:outline-none focus:ring focus:border-blue-500"
+                      name="alergias"
+                      type="text"
+                    />
                   </div>
 
-                  <h2 className={`${styles.titularLabel} relative`}>
-                    Contactos
-                  </h2>
-                  <div className="grid grid-cols-2 gap-4">
-                    {/* CARD CONTACT */}
-                    <CardContact />
+                  <div className="derecha">
+                    <div className="redondo  gap-2 flex">
+                      <Image alt="redondo" src={redondo}></Image>
+                      <label htmlFor="">Tratamiento</label>
+                    </div>
 
-                    {/* CARD CONTACT */}
-
-                    <CardContact />
+                    <input
+                      onChange={(e) => handleChangeDatos(e)}
+                      className="w-36  py-1 rounded-md border border-gray-300 focus:outline-none focus:ring focus:border-blue-500"
+                      name="tratamiento"
+                      type="text"
+                    />
                   </div>
                 </div>
 
-                <div
-                  className={`${styles.config} absolute top-0 right-0 mt-2 gap-2`}
-                >
-                  <button
-                    onClick={handleSubmit}
-                    className={`${styles.botonSave}`}
-                  >
-                    Guardar
-                  </button>
-
-                  <Image
-                    className="cursor-pointer"
-                    onClick={toggleForm}
-                    alt="cerrar"
-                    src={close}
-                  ></Image>
-                  <Image
-                    className="cursor-pointer"
-                    alt="mas info"
-                    src={tres}
-                  ></Image>
+                <h2 className={`${styles.titularLabel} relative`}>
+                  Alimentacion
+                </h2>
+                <div className="w-full">
+                  <textarea
+                    onChange={(e) => handleChangeTextArea(e)}
+                    className={`${styles.textarea}`}
+                    name="mensaje"
+                    id="mensaje"
+                  ></textarea>
                 </div>
+
+                <h2 className={`${styles.titularLabel} relative`}>Contactos</h2>
+                <div className="grid grid-cols-2 gap-4">
+                  {/* CARD CONTACT */}
+                  <CardContact />
+
+                  {/* CARD CONTACT */}
+
+                  <CardContact />
+                </div>
+
+                {alerta && (
+                  <div className="bg-red-200 mt-2 mx-auto w-full flex justify-center text-red-800 px-4 py-2 rounded-md mb-4">
+                    <p className="text-sm text-center md:text-base">
+                      ¡Faltan campos por completar! Por favor, verifica los
+                      campos obligatorios.
+                    </p>
+                  </div>
+                )}
               </div>
-            </Formulario>
-          </>
-        )}
-      </div>
-  
+
+
+                <Controlador handleSubmit={handleSubmit}
+                             toggleForm={toggleForm}
+                
+                />
+
+
+
+            </div>
+
+          </form>
+
+
+
+          
+
+          </Formulario>
+     
+      
+        </>
+      )}
+    </div>
   );
 };
