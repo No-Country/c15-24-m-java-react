3d5e20e2561280e328da3ca6c2ee53838a56367b
Nahuel Benitez <nahueljosebenitez7@gmail.com>
2023-12-20 15:41:16 -0300

    commit merge segunda-nico

--- Frontend/pet-app/src/app/(main)/layout.tsx
+++ Frontend/pet-app/src/app/(main)/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
-import { Nunito } from "next/font/google";
+import { Inter } from "next/font/google";
 import "../../app/globals.css";
 import { Navbar } from "../../components/navbar/navbar";
 import { ContainerMain } from "../../components/containerMain/containerMain";
 import { NavbarSup } from "@/components/navbarSuperior/navbarSup";
-
-const nunito = Nunito({ subsets: ["latin"] });
+import { DataProvider } from "@/components/Context/DataContext";
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Pet App",
