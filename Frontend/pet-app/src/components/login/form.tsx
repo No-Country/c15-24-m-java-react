3081b65031cf51814ec1b1d6f82f11f0886bdfcf
Nahuel Benitez <nahueljosebenitez7@gmail.com>
2023-12-21 14:12:45 -0300

    styles: backgroun wallpaper

--- Frontend/pet-app/src/components/login/form.tsx
+++ Frontend/pet-app/src/components/login/form.tsx
@@ -16,6 +16,20 @@ const Form = () => {
         <h2 className="text-secundario3 text-xl">Ingrese sus datos</h2>
       </div>
       <form className="w-full">
+        <input
+          type="email"
+          className="bg-gray-100 w-full py-3 pl-4 pr-12 outline-none rounded-xl mb-5"
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          className="bg-gray-100 w-full py-3 pl-4 pr-12 outline-none rounded-xl"
+          placeholder="Password"
+        />
+        <button className="w-full bg-secundario2 text-grey py-3 px-4 rounded-xl mb-5 border-2 border-transparent hover:bg-transparent hover:border-secundario2 hover:text-black transition-colors duration-300 mt-3 ">
+          Login
+        </button>
+
         {/* <Input type="text" placeholder="Email" />
         <Input type="password" placeholder="Password" /> */}
         {/* <Button type="submit" label="Login" /> */}
